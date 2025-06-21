1a3205f51639fa555d28fc286aa0b39d40a02e3a
josue <josue.mlpro.2014@gmail.com>
2025-06-21 18:31:13 -0500

    ejercicio 1- avance 1- estructura casi terminada de convetir la primera letra a mayuscula

---
+++ ejercicio1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<cctype>
+
+using namespace std;
+
+int main()
+{
+string texto;
+
+cout<<ingrese una oracion en minuscula <<endl;
+getline (cin, texto);
+
+int letra=1; 
+
+for(size_t i=0; i< texto.length(); i++){
+
+if(isspace(texto[i])){
+
+letra= 1;
+}else if(letra ==1 ){
+
+texto[i] = toupper(texto[i]);
+letra=0;
+}
+}
+
+cout<<la oracion nueva es: <<endl;
+cout<<endl;
+cout<< texto;
+    
+    return 0;
+} 
