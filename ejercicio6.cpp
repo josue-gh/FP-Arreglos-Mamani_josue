4709c46e87ceee908bc547d46c2108dfa9425f47
josue <josue.mlpro.2014@gmail.com>
2025-06-22 11:17:36 -0500

    ejercicio 6-version final-agregando datos al vector y calculando la suma de indice par e impar

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -5,9 +5,25 @@ using namespace std;
 
 int main(){
 	
+	int  sumP=0, sumI=0;
+
+vector<int> num= {12, 11, 9, 8, 3, 5, 4, 13};
+
+for(int i=0; i<num.size(); i++){
 	
+if(i %2==0){
+sumP=sumP+ num[i];
+
+}else{
 	
-	
-	
+sumI=sumI+ num[i];
+}
+
+}
+
+cout<<"la suma de numeros de indice par es: "<<sumP<<endl;
+cout<<endl;
+cout<<"la suma de numeros de indice impar es: "<<sumI<<endl;
+
 	return 0;
 } 
