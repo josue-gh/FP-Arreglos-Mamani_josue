b9e7b3bf17fcf8b37ac6f647d1bf36cef6d4436b
josue <josue.mlpro.2014@gmail.com>
2025-06-21 22:25:03 -0500

    ejercicio 4-version final-nombres en odern alfabetico

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -6,9 +6,40 @@ using namespace std;
 int main(){
 	
 	
-	int cantidad;
-	string nombre, auxiliar;
+	int i, j, cantidad;
+	string  auxiliar;
 	
-	for()
+	cout<<"cuantos nombres va a ingresar "<<endl;
+	cin>> cantidad;
+	cin.ignore();
+	
+	cout<<endl;
+	cout<<"ingrese los nombres: "<<endl;
+	
+	string nombre[cantidad];
+	
+	for (i=0; i<cantidad; i++){
+		
+		getline(cin, nombre[i]);
+	}
+	
+	for(i=0; i<cantidad-1; i++){
+		
+		for(j=i+1; j<cantidad; j++){
+		
+		if(nombre[i][0]> nombre[j][0] ){
+		 swap(nombre[i], nombre[j]);
+		}
+		}
+	}
+	
+	cout<<endl;
+	
+	cout<<"los nombres en orden alfabetico son: "<<endl;
+	cout<<endl;
+	for(i=0; i<cantidad; i++){
+		
+		cout<<nombre[i]<<endl;
+	}
  
 } 
