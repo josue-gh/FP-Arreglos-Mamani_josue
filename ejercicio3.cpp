99e9b04c7146d99fe32cab0eb8fa1e3078918171
josue <josue.mlpro.2014@gmail.com>
2025-06-21 21:04:44 -0500

    ejercicio 3-version final-hallando la ocurrencia de una letra en una oracion

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -5,12 +5,35 @@ using namespace std;
 
 int main(){
 	
+	int i, contador;
 	string palabra;
 	char letra;
-	cout<<ingrese la oracion <<endl;
+	
+	contador=0; 
+	
+	cout<<"ingrese la oracion "<<endl;
 	
 	getline(cin, palabra);
 	
-	cout<<ingrese la letra que desea contar su ocurrencia <<endl;
+	cout<<endl;
+	cout<<"ingrese la letra que desea contar su ocurrencia "<<endl;
 	cin>> letra;
+	cout<<endl;
+	//realiza un bucle for que empieza desde 0 hasta la longitud de la palabra
+	
+	for(i=0; i<palabra.length(); i++){
+		
+		// realiza una condicion de si la letra elegida por el usuario es igual a la primera letra del arreglo
+		if(letra==palabra[i]){
+			
+			//si es verdad el contador aumenta en 1
+			contador ++;
+		}
+	}
+	
+	cout<<"la cantidad de "<< letra <<" que hay es: "<<endl;
+	
+	cout<<contador;
+	
+	return 0;
 } 
