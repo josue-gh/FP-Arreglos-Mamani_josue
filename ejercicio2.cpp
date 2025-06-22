ce45be6f3c846d1a32b2d36e97aebb0dbc76b2e0
josue <josue.mlpro.2014@gmail.com>
2025-06-21 20:37:05 -0500

    ajercicio 2-version final-poniendo cada palabra en una linea separada

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,4 +1,28 @@
 #include<iostream>
 #include<string>
+#include<cctype>
 
 using namespace std; 
+
+int main(){
+	
+	string palabra;
+	
+	cout<<"ingrese una oracion "<<endl;
+	
+	getline (cin, palabra);
+	
+	cout<<endl;
+	
+	for(size_t i=0; i<palabra.length(); i++){
+		
+	if(isspace(palabra[i])){
+		
+		cout<< endl;
+	}else{
+		
+		cout<< palabra[i];
+	}
+	}
+	
+}
