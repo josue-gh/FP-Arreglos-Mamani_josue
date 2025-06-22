135be4b3a227890ca45d3d74fcd65250130fa032
josue <josue.mlpro.2014@gmail.com>
2025-06-22 15:20:44 -0500

    ajercicio 8-version final-pidiendo datos y realizando bucles para determinar si el numero es capicua

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,10 +1,36 @@
 #include<iostream>
 #include<vector>
+#include<string>
 
 using namespace std;
 
 int main(){
 	
+	string num;
+int dis=0;
+bool esCapicua= true;
+cout<<"ingrese un numero de 10 cifras "<<endl;
+
+getline(cin, num);
+
+
+//creamos un vector llamado digitos y dentro realizamos un iterador inicial
+//llamado begin() junto con end() para colocar cada digito de tipo char en el vector
+vector<char> digitos(num.begin(), num.end());
+
+for(int i= 0; i<5; i++)
+if ( digitos[i] != digitos[9-i]){
+esCapicua= false;
+break;
+}
+
+if( esCapicua==true){
+cout<<"el numero es Capicua "<<endl;
+}else if(esCapicua==false){
+cout<<"el numero no es Capicua "<<endl;
+}
+
+return 0;
 	
 	
 	return 0;
