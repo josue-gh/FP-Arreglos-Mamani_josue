8ff0344b72c2987256b91eabbc58fcb55b2a84ff
josue <josue.mlpro.2014@gmail.com>
2025-06-22 09:54:07 -0500

    ejercicio 5-version final-corrigiendo errores y terminando el codigo

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -7,18 +7,21 @@ int main()
 {
 int  cantidad, aux;
 
-cout<< ingrese la cantidad de numeros<<endl;
+cout<<"ingrese la cantidad de numeros "<<endl;
 cin >> cantidad;
 
+//arreglo de los numeros dados por el usuario
 int num[cantidad];
 
-cout<<ingrese los numeros <<endl;
+cout<<"ingrese los numeros "<<endl;
+//bucle que empieza de 0 hasta la cantidad de numeros -1
 
 for(int i=0; i< cantidad; i++){ 
 
 cin>> num[i];
 }
 
+//parte en donde ordenamos la numeros de menor a mayor
 for(int i=0; i<cantidad -1; i++){
 for(int j=i+1; j<cantidad; j++){
 if( num[i] > num[j]){
@@ -31,10 +34,10 @@ num[j] = aux;
 }
 
 cout<<endl;
-
-cout<<  el numero mayor es:  << num[cantidad-1];
+//retornando el numero mayor y menor
+cout<<"  el numero mayor es:  "<< num[cantidad-1];
 cout<<endl;
-cout<<el numero menor es: <<num[0];
+cout<<"el numero menor es: "<<num[0];
 
 return 0;
 
