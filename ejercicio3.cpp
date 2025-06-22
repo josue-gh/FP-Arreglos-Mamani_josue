098cd6557c165fe2975173e6e007838a2fb42639
josue <josue.mlpro.2014@gmail.com>
2025-06-21 20:50:18 -0500

    ejercicio 3-avance 1-pidiendo datos al usuario

---
+++ ejercicio3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	
+	string palabra;
+	char letra;
+	cout<<ingrese la oracion <<endl;
+	
+	getline(cin, palabra);
+	
+	cout<<ingrese la letra que desea contar su ocurrencia <<endl;
+	cin>> letra;
+} 
