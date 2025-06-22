84a91c061e6d1e5b4f9bbc9677d92b71b8580610
josue <josue.mlpro.2014@gmail.com>
2025-06-22 12:49:49 -0500

    ajercicio 7-avance 1- otorgando datos a un vector y realizando bucles

---
+++ ejercicio7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> num = {12, 15, 18, 21, 23, 55};
+
+    cout << Los numeros son: << endl;
+    for (int i = 0; i < num.size(); i++) {
+        cout << num[i] << endl;
+    }
+
+    cout << endl;
+    cout << Los numeros en orden invertido son: << endl;
+    for (int i = num.size() - 1; i >= 0; i--) {
+        cout << num[i] << endl;
+    }
+
+    return 0;
+} 
