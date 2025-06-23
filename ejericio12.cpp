ff8d32c74f5f16979cc441721f3b72ceb3229493
josue <josue.mlpro.2014@gmail.com>
2025-06-23 09:55:57 -0500

    ejercicio 11-version final- corrigiendo errores y mejorando la legibilidad

--- ejericio12.cpp
+++ ejericio12.cpp
@@ -1,37 +1,60 @@
 #include<iostream>
 #include<vector>
+
 using namespace std;
+
 bool ordenado(int longitud, vector<int>& elementos){
+	
 bool ascendente= true;
 bool descendente = true ;
+
 for(int i=0; i<longitud; i++){
+
 if( elementos[i] < elementos[i+1]){
 descendente= false;
+
 }else if(elementos[i] > elementos[i+1]){
 ascendente = false;
+
 }
+
 }
+
 if(ascendente and descendente ==false){
+
 return false;
+
 }else {
+
  return true;
 }
+
 }
+
+
 int main(){
+
 int can, num; 
 bool orden;
+
 vector<int>numero ;
-cout<<ingrese la cantidad de numeros <<endl;
+
+cout<<"ingrese la cantidad de numeros "<<endl;
 cin>> can;
-cout<<ingrese los numeros <<endl;
+
+cout<<"ingrese los numeros "<<endl;
 for(int i=0; i<can; i++){
 cin>>num;
 numero.push_back(num);
 }
+
 orden= ordenado(can, numero);
+
 if( orden== true){
-cout<<los numeros ingresados estan ordenados<<endl;
+cout<<"los numeros ingresados estan ordenados "<<endl;
 }else{
-cout<<los numeros ingresados no estan ordenados<<endl;
+cout<<"los numeros ingresados no estan ordenados "<<endl;
 }
-return 0;} 
+
+return 0;
+} 
